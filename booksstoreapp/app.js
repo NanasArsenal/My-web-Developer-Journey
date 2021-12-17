7f1a30926b4adfd3e1e42e31aa7118e96c7063c2
Nana Kweku Otoo <77609447+NanasArsenal@users.noreply.github.com>
2021-12-17 15:31:52 +0000

    a simple app to keep record of books

---
+++ booksstoreapp/app.js
@@ -0,0 +1,38 @@
+const bookTitle = document.getElementById('book-title');
+const bookNumber = document.getElementById('book-number');
+//get the table body
+const tableBody= document.querySelector('.table-body');
+console.log(tableBody);
+
+
+document.getElementById('book-form').addEventListener('submit', createBooktable);
+
+function createBooktable(e){
+
+    //creating table row element
+    const trow =document.createElement('tr');
+    trow.className='table-row';
+
+    //creating an element for the booknumber
+    const booknumb= document.createElement('th');
+    booknumb.appendChild(document.createTextNode(bookNumber.value));
+//appending book number to the trow
+    trow.appendChild(booknumb);
+
+
+    //creating an element for the booktitle column
+    const bookname= document.createElement('td');
+    bookname.appendChild(document.createTextNode(bookTitle.value));
+
+    //appending book title to the trow
+    trow.appendChild(bookname);
+
+    tableBody.appendChild(trow);
+
+
+
+
+
+
+    e.preventDefault();
+}
